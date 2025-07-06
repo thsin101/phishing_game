cde21358d8017e1480c11b623930ef66c9162f89
Thomas Sinner <thomas-sinner@gmx.de>
2025-07-06 21:05:30 +0200

    Configure vite github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/phishing_game/', // Diese Zeile hinzufügen
   plugins: [react()],
 })
